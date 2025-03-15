fc62db846d53aae29ec6623bcbe9310d8a8754d4
Anthony Accioly <1591739+aaccioly@users.noreply.github.com>
2025-03-15 00:46:38 +0000

    feat(styles): enhance layout and typography

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,16 @@
 const toggle = document.getElementById('dark-mode-toggle');
-const body = document.body;
+const html = document.documentElement;
 
 // Check for saved user preference
 if (localStorage.getItem('darkMode') === 'enabled') {
-    body.classList.add('dark-mode');
+    html.classList.add('dark-mode');
 }
 
 toggle.addEventListener('click', () => {
-    body.classList.toggle('dark-mode');
+    html.classList.toggle('dark-mode');
 
     // Save user preference
-    if (body.classList.contains('dark-mode')) {
+    if (html.classList.contains('dark-mode')) {
         localStorage.setItem('darkMode', 'enabled');
     } else {
         localStorage.setItem('darkMode', 'disabled');
